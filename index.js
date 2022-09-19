5e3e9805c8871cf181f13765a3fab6cf3aba5bc2
Bo Brockmann <bbrockmann1@gmail.com>
2022-09-19 15:25:36 -0600

    commit tree c02a0aa86b50cc4eab6d3d14080987ccc7c6297e

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    return Math.abs(42-distance);
+};
+
+const distanceFromHqInFeet = distance => {return Math.abs(42-distance) * 264;}
+
+const distanceTravelledInFeet = (start, end) => {
+    return Math.abs(start-end) *264
+};
+
+const calculatesFarePrice = (start, end) => {
+    const distance = Math.abs(start - end) * 264;
+    switch (true){
+        case distance <= 400:
+            return 0;
+            break;
+
+        case distance <= 2000:
+            return (distance - 400) * .02;
+            break;
+
+        case distance <= 2500:
+            return 25;
+            break;
+
+        default: return 'cannot travel that far';    
+      
+    }
+
+};
